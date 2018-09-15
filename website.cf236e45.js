016e062e5d65a1e5fbbd658cbd4d690774f9270c
Jake Cooper <jake.elijah.cooper@gmail.com>
2018-09-15 00:12:28 -0400

    Updated dist

--- website.cf236e45.js
+++ website.cf236e45.js
@@ -25600,7 +25600,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '56540' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '56710' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
